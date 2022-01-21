a1d48b8660e57cd1f249703cebf697a00ccae4c5
NMarti20 <natasha.martinezsj@gmail.com>
2022-01-21 12:59:51 -0800

    added projects data file in assets. Imported dummy project image

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,14 @@
 import "./_projects.scss";
+import { Swiper } from "swiper";
 
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div className="projects">
+      <h3 className="projects__title">Projects</h3>
+
+      <div className="projects__allItems"></div>
+    </div>
+  );
 };
 
 export default Projects;
