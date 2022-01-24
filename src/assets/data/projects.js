80d9165c31a2b58168028dff71e601ac978a697a
NMarti20 <natasha.martinezsj@gmail.com>
2022-01-24 13:06:40 -0800

    created a project item js file for individual projects item

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -8,6 +8,13 @@ const projects = [
     desc: "Built my personal portfolio website, leveraging React, JS, HTML, CSS/SASS, Flexbox",
     img: { dummyProject },
   },
+
+  {
+    id: uuidv4(),
+    name: "BrainFlix",
+    desc: "Created a prototype for a new video streaming platform, leveraging React, JS, HTML, CSS/SASS, Flexbox, API, Postman, Axios, React Router, NodeJs, Express, Heroku",
+    img: { dummyProject },
+  },
   {
     id: uuidv4(),
     name: "Simultaneous Timers",
@@ -16,14 +23,14 @@ const projects = [
   },
   {
     id: uuidv4(),
-    name: "BrainFlix",
-    desc: "Created a prototype for a new video streaming platform, leveraging React, JS, HTML, CSS/SASS, Flexbox, API, Postman, Axios, React Router, NodeJs, Express",
+    name: "The Universe",
+    desc: "Built an app using NASA's APOD API, using React, JS, HTML, CSS/SASS",
     img: { dummyProject },
   },
   {
     id: uuidv4(),
     name: "BandSite",
-    desc: "Built a website for an 'up-and-coming' band, leveraging, JS, HTML, CSS/SASS, Flexbox, DOM manipulation, and API implementation using Heroku ",
+    desc: "Built a website for an 'up-and-coming' band, leveraging, JS, HTML, CSS/SASS, Flexbox, DOM manipulation, API, Heroku ",
     img: { dummyProject },
   },
   {
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,20 @@
 import "./_projects.scss";
-import { Swiper } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import projects from "../../assets/data/projects";
 
 const Projects = () => {
   return (
     <div className="projects">
       <h3 className="projects__title">Projects</h3>
 
-      <div className="projects__allItems"></div>
+      <div className="projects__allItems">
+        <Swiper>
+          {projects.map((project, index) => {
+            if (index >= 5) return;
+            return <div>This is my project</div>;
+          })}
+        </Swiper>
+      </div>
     </div>
   );
 };
