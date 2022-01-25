efc83e70e99f1f0eda9ee003e5c36dfe15f97327
NMarti20 <natasha.martinezsj@gmail.com>
2022-01-24 18:28:48 -0800

    added contact desc and inserted to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
+import ContactMe from "./components/ContactMe/ContactMe";
 import "./App.css";
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <AboutMe />
       <Skills />
       <Projects />
+      <ContactMe />
     </div>
   );
 }
--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function ContactMe() {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <div>
+        <h3>
+          I am currently looking for new opportunities! If you have requests or
+          questions, don't hesitate to reach out to me, and I'll try my best to
+          get back to you in a timely manner.
+        </h3>
+      </div>
+    </div>
+  );
 }
 
 export default ContactMe;
