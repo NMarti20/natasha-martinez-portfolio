b01eea8cbb9844e3fa316fb3e17518a8dda71ab3
NMarti20 <natasha.martinezsj@gmail.com>
2022-01-24 18:40:41 -0800

    populated contact me page

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./_contactMe.scss";
 
 function ContactMe() {
   return (
@@ -10,6 +11,23 @@ function ContactMe() {
           questions, don't hesitate to reach out to me, and I'll try my best to
           get back to you in a timely manner.
         </h3>
+        <div className="contactMe__icons">
+          <a href="https://github.com/NMarti20" className="home__icons-icon">
+            <i className="fab fa-github "></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/natasha-a-martinez/"
+            className="home__icons-icon"
+          >
+            <i className="fab fa-linkedin "></i>
+          </a>
+          <a
+            href="mailto:natasha.martinez1117@gmail.com"
+            className="home__icons-icon"
+          >
+            <i class="far fa-envelope"></i>
+          </a>
+        </div>
       </div>
     </div>
   );
